0abbe9cdd08c045c32746fdc0e96543cdb57bc24
Nirob Hasan <nirob427417@gmail.com>
2023-02-07 19:37:09 +0600

    get and post create

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
+        const orderCollection = client.db('geniusCar').collection('orders');
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -26,12 +27,21 @@ async function run() {
             res.send(services);
         });
 
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
+            const query = { _id: new ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+
     }
     finally {
 
